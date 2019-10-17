d4786f3b7c9a454a80a4195cd19e778ad4894d40
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2019-10-17 14:46:51 +0700

    Add files via upload

---
+++ IF1-10118040-Latihan36-(ObjectOriented)TargetSaldoTabunga/src/if1/pkg10118040/latihan36/objectoriented/targetsaldotabunga/TargetSaldo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118040.latihan36.objectoriented.targetsaldotabunga;
+
+/**
+ *
+ * @author asus
+ */
+public class TargetSaldo {
+    
+        public double saldoAwal;
+	public float bunga;
+	public double saldoAkhir;
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public void main(String args) {
+		// TODO - implement TargetSaldo.main
+		throw new UnsupportedOperationException();
+	}
+
+	public void hitungSaldo() {
+		// TODO - implement TargetSaldo.hitungSaldo
+		throw new UnsupportedOperationException();
+	}
+
+
+}
